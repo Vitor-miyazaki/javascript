07a1af499e994c2d949e05a06eaf5a99785aceb1
Vitor Yuri R Miyazaki <113317785+Vitor-miyazaki@users.noreply.github.com>
2024-10-22 10:21:04 -0300

    Pasta: Atribuindo classes e duplicando elementos

--- exercicios/aula 03 - DOM/atribuindo classes e duplicando elementos/index.js
+++ exercicios/aula 03 - DOM/atribuindo classes e duplicando elementos/index.js
@@ -1,51 +1,35 @@
-
-function clickHTML(){
-let html = document.querySelector('.html')
-html.setAttribute('class', 'importante')
-}
-
-function clickCSS(){
-    let css = document.querySelector('.css')
-    css.setAttribute('class', 'importante')
-}
-
-function clickJS(){
-    let js = document.querySelector('.javascript')
-    js.setAttribute('class', 'importante')
-}
-
-function clickPHP(){
-    let php = document.querySelector('.php')
-    php.setAttribute('class', 'importante')
-}
-
-function clickReact(){
-    let react = document.querySelector('.react')
-    react.setAttribute('class', 'importante')
-}
-
-function clickMysql(){
-    let mysql = document.querySelector('.mysql')
-    mysql.setAttribute('class', 'importante')
-}
-
-function clickCopiar(){
-    let selecionados = [document.querySelectorAll('div.importante')]
-    console.log(selecionados)
-    let sectionRight = document.querySelector('.right')
-    let txt = document.createElement('p')
-    txt.innerText = 'oooo'
-    let teste = selecionados.map(el=>el)
-    let criaçao = document.createElement(teste)
-    sectionRight.appendChild(criaçao)
-}
 /* 
-
-1 - Quando clicar o que vai acontecer? 
-a - mudar a cor --- como adicionar uma classe? var.setAttribute('classe','importante')
-b - ficar selecionado 
-
-2 - Como copiar e enviar para o outro lado? 
-3 - Como fazer isso para os demais de um forma otimizada? 
-
+1 - Colocando todos os elementos em uma array
+2 - Para cada elemento(MAP), haverá uma ação de click(addEventListener)
+3 - Nessa ação de click, vai adicionar uma classe chamada importante. Utilizando junto ao Toggle
+4 - O botão de copiar, terá um outro addEventListener
+5 - Ao clicar, ele irá selecionar somente as div com class importante e coloca-las em uma array
+6 - Para cada elemento, terá um appendChild na section da direita. 
 */
+
+
+const todosElementos = [...document.querySelectorAll('div.cursos')]
+const btnCopiar = document.querySelector('.copiar')
+const right = document.querySelector('.right')
+//console.log(btnCopiar)
+
+todosElementos.map(el=>{
+    el.addEventListener('click', ()=>{
+        el.classList.toggle('importante')
+    })
+})
+
+btnCopiar.addEventListener('click', ()=>{
+    const importante = [...document.querySelectorAll('div.importante')]
+    if(importante.length==0){
+        window.alert('Selecione um curso para prosseguir')
+    }else{
+    right.innerHTML = ''    
+    //console.log(importante)
+
+    importante.map(el=>{
+        right.appendChild(el)
+    })
+    }
+})
+
