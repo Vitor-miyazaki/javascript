c905b4dae81d8fbcefb3cf3506df9041cc6edeca
Vitor Yuri R Miyazaki <113317785+Vitor-miyazaki@users.noreply.github.com>
2024-10-26 10:19:34 -0300

    Registro de itens

--- exercicios/aula 03 - DOM/registro de itens/prog.js
+++ exercicios/aula 03 - DOM/registro de itens/prog.js
@@ -1,31 +1,37 @@
 const texto = document.querySelector('input#texto')
 const btn_salvar = document.querySelector('input#salvar') 
 const cx_conteudo = document.querySelector('.conteudo')
-let arr = []
-let ex = 'texto'
+const vazio = document.querySelector('.vazio')
+let arr = ['aaaa','bbb']
 
 
-btn_salvar.addEventListener('click', ()=>{
+/* 
+Para cada elemento adicionado, necesário vincular o onclick
+Ao clicar, vou remover o elemento
+*/
+
+btn_salvar.addEventListener('click', ()=>{  
     if(texto.value==''){
         window.alert('[ERRO] Valor não encontrado. Por favor, digite o item que deseja salvar!')
     }else{
     //window.alert(texto.value)
-    arr.push(texto.value)
-    texto.value = ''
+    vazio.style.display = 'none'
+    arr.push(texto.value)       
+    const novoItem = document.createElement('div')
+    novoItem.setAttribute('class','itens')
+    novoItem.innerText = texto.value
+    cx_conteudo.appendChild(novoItem)
+    texto.value = '' 
     
-    arr.map(el=>{
-        //O elemento é o conteúdo da array
-        //Cade elemento precisa gerar uma div por meio do createElement
-        //Adicionar atributo classe
-        //Fazer com que apareça na section
-        const novoItem = document.createElement('div')
-        novoItem.setAttribute('class','itens')
-        novoItem.innerText = el
-        cx_conteudo.appendChild(novoItem)
-    })
-
     
-}
+        /* arr.map(el=>{
+            el.addEventListener('click', ()=>{
+            console.log('teste')
+            })
+        }) */
+    
+    
+    }
 })
 
 
