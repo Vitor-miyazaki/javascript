370c5875f518993a2d6bb269272845c666bc2ac1
Vitor Yuri R Miyazaki <113317785+Vitor-miyazaki@users.noreply.github.com>
2024-11-13 19:42:17 -0300

    pasta treino

--- exercicios/treino/treino.js
+++ exercicios/treino/treino.js
@@ -1,2 +1,35 @@
 "use strict"
 
+const inicio = document.querySelector('#inicio')
+const fim = document.querySelector('#fim')
+const passo = document.querySelector('#passo')
+const btn_contar = document.querySelector('#btn_contar')
+const txt = document.querySelector('.txt')
+
+btn_contar.addEventListener('click', ()=>{
+    
+
+    if(inicio.value==''||fim.value==''|| passo.value==''){
+        alert('Digite em todos os campos!')
+    }else{
+        const i = Number(inicio.value)
+        const f = Number(fim.value)
+        const p = Number(passo.value)
+
+        txt.innerHTML = 'Resultado: '
+
+        if(i<=f){
+            for(let c=i; c<=f; c+=p){
+                txt.innerHTML += `${c} \u{1F449}`
+            }
+
+        }else{
+            for(let c=i; c>=f; c-=p){
+                txt.innerHTML += `${c} \u{1F449}`
+            }
+            
+        }
+        
+        txt.innerHTML += `FIM!`
+    }
+})
