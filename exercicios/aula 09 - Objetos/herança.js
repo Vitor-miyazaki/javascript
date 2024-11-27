06424e45f6e91a22483f277c2dece81d56ebf664
Vitor Yuri R Miyazaki <113317785+Vitor-miyazaki@users.noreply.github.com>
2024-11-27 11:44:57 -0300

    Pasta Objetos

---
+++ exercicios/aula 09 - Objetos/herança.js
@@ -0,0 +1,58 @@
+class Pessoas{
+    constructor(nome,idade){
+        this.nome = nome
+        this.idade = idade
+    }
+    apresentar(){
+        console.log(`Olá, meu nome é ${this.nome}!`)
+        console.log(`Tenho ${this.idade} anos.`)
+    }
+}
+
+class Aluno extends Pessoas{
+    constructor(nome,idade,nota){
+    /*  this.nome = nome        Retirado, pois ambas as subclasses, possuem as mesmas propriedades, portanto é 
+        this.idade = idade      adicionado na classe pai  */
+        super(nome,idade)
+        this.nota = nota
+    }
+    apresentarAluno(){
+        super.apresentar()
+        console.log(`Tirei ${this.nota} pontos na prova.`)
+    }
+
+}
+
+class Professor extends Pessoas{
+    constructor(nome,idade,materia){
+    /*  this.nome = nome        Retirado, pois ambas as subclasses, possuem as mesmas propriedades, portanto é 
+        this.idade = idade      adicionado na classe pai  */
+        super(nome,idade)
+        this.materia = materia
+    }
+
+    apresentarProfessor(){
+        super.apresentar()
+        console.log(`Dou aula de ${this.materia}.`)
+
+    }
+
+}
+
+const aluno1 = new Aluno('Vitor', 29, 6)
+const professor1 = new Professor('Bruno', 45, 'programação')
+
+/* console.log(aluno1.nome)
+console.log(aluno1.idade)
+console.log(aluno1.nota)
+
+console.log(professor1.nome)
+console.log(professor1.idade)
+console.log(professor1.materia) 
+
+Isso está repetindo muito e poluindo o código, portanto podemos colocar como método e personalizar as informações. O que é repetido, será da classe pai. 
+*/
+
+aluno1.apresentarAluno()
+console.log('-------')
+professor1.apresentarProfessor()
--- exercicios/aula 09 - Objetos/objeto1.js
+++ exercicios/aula 09 - Objetos/objeto1.js
@@ -1,20 +1,5 @@
 "use strict"
-
 /* 
-Matriz: 
-
-let arr = ['texto', 3, true]
-console.log(arr)
-console.log(arr[0]) 
-
-Não contém funções
-Seus índices são fixos em números, não podemos colocar identificadores personalizados
-ruim para projetos extensos
-*/
-
-
-//objeto --- {string:'texto, numero: 10, bolean: true, funções}
-
 let amigo = {
 nome: 'José', //precisa ter virgulas
 peso: 85.4,  // Precisa ter ponto
@@ -24,4 +9,34 @@ engordar (p = 0){ // é uma função, porém não podemos utilizar 'function' na
 }}
 
 amigo.engordar(2)
-console.log(`${amigo.nome} pesa ${amigo.peso} Kg`)
+console.log(`${amigo.nome} pesa ${amigo.peso} Kg`) */
+
+
+const Pessoa = {
+    nome: 'Vitor',
+    idade: 29,
+         
+    getNome: function(){
+        return this.nome
+    },
+
+    setNome: function(nome){
+        this.nome = nome
+    }
+}
+
+const p2 = Pessoa //Aponta para o mesmo obj
+const p3 = Pessoa //Aponta para o mesmo obj
+
+
+//3 formas de alterar as propriedades do obj
+p3.nome = 'Yuri'
+p2['nome'] = 'Miyazaki'
+Pessoa.setNome('Rossarolla')
+
+//3 formas de chamar a propriedade do obj
+console.log(Pessoa.nome) //Rossarolla
+console.log(p2.getNome()) //Rossarolla
+console.log(p3.nome) //Rossarolla
+
+
