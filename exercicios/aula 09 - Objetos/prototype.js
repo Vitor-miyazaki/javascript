b08ad94c6130c505430a49a36640c8577c311c96
Vitor Yuri R Miyazaki <113317785+Vitor-miyazaki@users.noreply.github.com>
2025-01-24 09:45:03 -0300

    prototype

--- exercicios/aula 09 - Objetos/prototype.js
+++ exercicios/aula 09 - Objetos/prototype.js
@@ -7,6 +7,11 @@
 - Pode ser utilizado de maneira dinâmica(durante a execução do programa). 
 */
 
+
+
+/* 
+Exercício 01
+
 function Pessoa(nome,idade,cidade){
     this.nome = nome
     this.idade = idade
@@ -33,5 +38,41 @@ p1.apresentar()
 console.log('----')
 
 p2.apresentar()
-p3.apresentar()
+p3.apresentar() */
 
+/* Exercício 02 */
+
+function Veiculo(marca,modelo,ano){ //Construtor Pai ---> para propriedades
+    this.marca = marca
+    this.modelo = modelo
+    this.ano = ano
+}
+
+Veiculo.prototype.descricao = function(){ //1° protótipo do pai ---> para métodos 
+    console.log(`Marca: ${this.marca}, Modelo: ${this.modelo}, Ano: ${this.ano}.`)
+}
+
+Veiculo.prototype.velocidadeInicial = function(){ //2° protótipo do pai
+    console.log(`A velocidade inicial é de 0 km/h`)
+}
+
+function Carro(marca,modelo,ano){ //Cronstrutor filho ---> para herdar propriedades do pai
+    Veiculo.call(this,marca,modelo,ano)
+    this.velocidade = 0  //Somente no filho, tem a propriedade de velocidade
+}
+
+Carro.prototype = Object.create(Veiculo.prototype) //Copiando os protótipos do pai, que são os métodos
+Carro.prototype.constructor = Carro //Estou apontando que o protótipo de Carro é do próprio carro. Sem isso, o sistema pode apontar que o protótipo de carro, é o veículo(pai). Isso pode gerar alguns problemas de organização e depuração. 
+
+//Quando você cria um objeto com "new", ele aponta para o local que o gerou (construtor). Se a instância aponta para Carro, então o local que o gerou também deve ser Carro. 
+
+Carro.prototype.acelerar = function(){ //Criei um método exclusivo para o filho
+    this.velocidade+=10
+    console.log(`A velocidade atual é de ${this.velocidade} km/h`)
+}
+
+const c1 = new Carro('Fiat','Uno',2015)
+c1.descricao()
+c1.velocidadeInicial()
+c1.acelerar()
+c1.acelerar()
